e8b0983ee27e17af0e68c923ace81e255df06128
Anna Bychkova <bychkova.dev@gmail.com>
2022-04-08 17:15:25 +0200

    bugfix/result-routes-and-manage-users-table-08/04/2022

--- web/src/screens/platform-page/components/manage-users/components/groups-data/groups-data.tsx
+++ web/src/screens/platform-page/components/manage-users/components/groups-data/groups-data.tsx
@@ -2,12 +2,17 @@ import { FC, useState } from 'react';
 
 import { Icon } from '@components/icon';
 
+import { useAppSelector } from '@services/hooks/redux';
+
+import { ROLES } from '@constants/user-roles';
+
 import { GroupsDataStyles as Styled } from './groups-data.styles';
 
 export const GroupsData: FC<{
   groups: IGroupForUser[];
   allUsers: IUserExistingAndInvited[];
 }> = ({ groups, allUsers }) => {
+  const { user } = useAppSelector((state) => state);
   const [showGroups, setShowGroups] = useState(false);
   const toggleShowModal = () => {
     setShowGroups((prev) => !prev);
@@ -16,7 +21,12 @@ export const GroupsData: FC<{
   return (
     <Styled.GroupDataColumnWrapper>
       {!groups.length && '-'}
-      {groups.length === 1 && groups[0].name}
+      {groups.length === 1 &&
+        user.role === ROLES.trainerAdmin &&
+        groups[0].name}
+      {groups.length === 1 &&
+        user.role === ROLES.superAdmin &&
+        `${groups[0].name} (${groups[0].trainerName})`}
       {groups.length > 1 && (
         <Styled.GroupsDiv onClick={toggleShowModal}>
           {groups[0].name} ({groups[0].trainerName})
@@ -24,13 +34,9 @@ export const GroupsData: FC<{
           {showGroups && (
             <Styled.GroupsModal>
               {groups.map((group) => {
-                const trainer = allUsers.find(
-                  (user) => user.id === group.trainerId
-                );
-                const trainerName = `${trainer?.firstName} ${trainer?.lastName}`;
                 return (
-                  <Styled.GroupText key={group.name}>
-                    {group.name} ({trainerName})
+                  <Styled.GroupText key={group.trainerName}>
+                    {group.name} ({group.trainerName})
                   </Styled.GroupText>
                 );
               })}
