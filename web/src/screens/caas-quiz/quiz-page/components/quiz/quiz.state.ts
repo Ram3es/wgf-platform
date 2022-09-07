0bb0cde887d20726a6e6cbffbd6e06e1e3a32386
Roman Luzan <romaluzan1@gmail.com>
2022-09-07 15:37:41 +0300

    prod/fix/subscriber-redirected-wrong-page

--- web/src/screens/caas-quiz/quiz-page/components/quiz/quiz.state.ts
+++ web/src/screens/caas-quiz/quiz-page/components/quiz/quiz.state.ts
@@ -185,7 +185,7 @@ export const useQuizState = () => {
         );
 
         if (user.isSubscriber) {
-          push(ROUTES.careerDesignCanvasResults);
+          push(ROUTES.careerFlexResults);
         } else {
           updateState({
             isShowModal: true,
